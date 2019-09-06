4939918776f36b4c34a1febf8af8b2e57b9fe548
Colin Starger <colinstarger@gmail.com>
2019-09-06 12:28:31 -0400

    first commit for generic_viz_manager.js - right now this is empty and is just a place where future javascript could go

---
+++ generic_viz_manager.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  // jQuery methods go here...
+  //WISH_LIST - when end_year selected, confirm it is not before begin_year
+  // Doesn't seem like anything is happening here. Could be unnecessary legacy
+
+});
